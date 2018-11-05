aa310e1eb984cb4553901db7782d1fb64ddd3cfb
Jason Wu <lujason2015@gmail.com>
2018-11-05 11:51:32 -0500

    Create zombiefeeding.sol

---
+++ zombiefeeding.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.19;
+
+import "./zombiefactory.sol";
+
+// Create KittyInterface here
+
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
+  }
+
+  
+
+}
