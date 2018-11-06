7e3d0fb1350e06d6f0ff5c485e67ab031011d2a2
Jason Wu <lujason2015@gmail.com>
2018-11-06 12:36:32 -0500

    Update zombiefactory.sol

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,38 +1,49 @@
 pragma solidity ^0.4.19;
 
-contract ZombieFactory {
+import "./ownable.sol";
+import "./safemath.sol";
 
-    event NewZombie(uint zombieId, string name, uint dna);
+contract ZombieFactory is Ownable {
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+  using SafeMath for uint256;
 
-    struct Zombie {
-        string name;
-        uint dna;
-    }
+  event NewZombie(uint zombieId, string name, uint dna);
 
-    Zombie[] public zombies;
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+  uint cooldownTime = 1 days;
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
+  struct Zombie {
+    string name;
+    uint dna;
+    uint32 level;
+    uint32 readyTime;
+    uint16 winCount;
+    uint16 lossCount;
+  }
 
-    function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
-        NewZombie(id, _name, _dna);
-    }
+  Zombie[] public zombies;
 
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(_str));
-        return rand % dnaModulus;
-    }
+  mapping (uint => address) public zombieToOwner;
+  mapping (address => uint) ownerZombieCount;
 
-    function createRandomZombie(string _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
-        uint randDna = _generateRandomDna(_name);
-        _createZombie(_name, randDna);
-    }
+  function _createZombie(string _name, uint _dna) internal {
+    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+    zombieToOwner[id] = msg.sender;
+    ownerZombieCount[msg.sender]++;
+    NewZombie(id, _name, _dna);
+  }
+
+  function _generateRandomDna(string _str) private view returns (uint) {
+    uint rand = uint(keccak256(_str));
+    return rand % dnaModulus;
+  }
+
+  function createRandomZombie(string _name) public {
+    require(ownerZombieCount[msg.sender] == 0);
+    uint randDna = _generateRandomDna(_name);
+    randDna = randDna - randDna % 100;
+    _createZombie(_name, randDna);
+  }
 
 }
